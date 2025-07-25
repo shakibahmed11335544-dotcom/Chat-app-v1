408609cd24baf6241c5ade62d5b53a1f88bccab4
Sakib <gshakib91@gmail.com>
2025-07-25 21:27:48 +0600

    Initial commit

--- server.js
+++ server.js
@@ -1,32 +1,51 @@
 const express = require('express');
 const http = require('http');
-const { Server } = require("socket.io");
+const { Server } = require('socket.io');
+const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-const PORT = process.env.PORT || 3000;
+// Serve static files (e.g., index.html)
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.static('public'));
-
+// Socket.io logic
 io.on('connection', (socket) => {
-  console.log('User connected:', socket.id);
+  console.log('A user connected');
 
   socket.on('join_room', (room) => {
     socket.join(room);
-    console.log(`User ${socket.id} joined room ${room}`);
+    console.log(`User joined room: ${room}`);
   });
 
   socket.on('chat_message', (data) => {
     io.to(data.room).emit('chat_message', data.message);
   });
 
+  socket.on('ready', (room) => {
+    socket.to(room).emit('ready');
+  });
+
+  socket.on('offer', (data) => {
+    socket.to(data.room).emit('offer', data);
+  });
+
+  socket.on('answer', (data) => {
+    socket.to(data.room).emit('answer', data);
+  });
+
+  socket.on('ice_candidate', (data) => {
+    socket.to(data.room).emit('ice_candidate', data);
+  });
+
   socket.on('disconnect', () => {
-    console.log('User disconnected:', socket.id);
+    console.log('A user disconnected');
   });
 });
 
+// Start server
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
