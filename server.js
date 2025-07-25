6af75cd98482337ed6d0bbe0e7da079ccbcbd5d5
Sakib <gshakib91@gmail.com>
2025-07-25 22:01:07 +0600

    Updated chat app with video/audio call features

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
+const port = process.env.PORT || 3000;
+
 app.use(express.static('public'));
 
-io.on('connection', socket => {
+io.on('connection', (socket) => {
   console.log('a user connected:', socket.id);
 
-  socket.on('join_room', room => {
+  socket.on('join_room', (room) => {
     socket.join(room);
     console.log(`User ${socket.id} joined room ${room}`);
   });
@@ -17,16 +19,8 @@ io.on('connection', socket => {
     socket.to(room).emit('chat_message', message);
   });
 
-  socket.on('video_offer', ({ room, sdp }) => {
-    socket.to(room).emit('video_offer', { sdp });
-  });
-
-  socket.on('video_answer', ({ room, sdp }) => {
-    socket.to(room).emit('video_answer', { sdp });
-  });
-
-  socket.on('ice_candidate', ({ room, candidate }) => {
-    socket.to(room).emit('ice_candidate', { candidate });
+  socket.on('signal', (data) => {
+    socket.to(data.room).emit('signal', data);
   });
 
   socket.on('disconnect', () => {
@@ -34,7 +28,6 @@ io.on('connection', socket => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
-http.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+http.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
 });
