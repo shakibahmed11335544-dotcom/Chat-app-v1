27a2e74e05a32dd40152dbf0a09236f56e081111
sakib667788 <your-email-associated-with-sakib667788>
2025-07-28 16:07:38 +0600

    S

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  root: path.resolve(__dirname), // client কে root ধরা হবে
+  root: path.resolve(__dirname),
   plugins: [react()],
   build: {
     outDir: "dist",
